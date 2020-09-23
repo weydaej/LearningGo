8cc0c70f2a47c7614e79564e4fa8f6ad1e2d5b18
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-09-23 16:49:54 -0400

    added some basics

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
-package main 
+package main
+
 import (
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	var x int = 3 // can also be written x := 3
+	x = 5
+	y := 1
+	sum := x + y
+	fmt.Println(sum)
+
+	var a [5]int
+	a[0] = 99
+	fmt.Println(a)
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(b)
+	c := []int{1, 2, 3, 4, 5}
+	c = append(c, 6)
+	fmt.Println(c)
+
+	vertices := make(map[string]int)
+	vertices["triangle"] = 3
+	vertices["square"] = 4
+	vertices["dodecagon"] = 12
+
+	fmt.Println(vertices)
+	fmt.Println(vertices["triangle"])
+	delete(vertices, "triangle")
+	fmt.Println(vertices)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 }
